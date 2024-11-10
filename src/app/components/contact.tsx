ae43f2361e5f3e95f0161e8c1edce4edc4334ed7
MaryamKhan2001 <maryamkhanhomm@gmail.com>
2024-11-10 17:22:57 +0500

    responsive

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -6,7 +6,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div className='ml-64' id='contact'>
+    <div  id='contact'>
       <section className="text-gray-600 body-font relative">
   <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
     <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
