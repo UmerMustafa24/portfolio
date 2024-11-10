14ad21253e9ef52920e0ef068d259228687cebfd
MaryamKhan2001 <maryamkhanhomm@gmail.com>
2024-11-10 17:29:33 +0500

    first commit

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -17,8 +17,7 @@ const Navbar = () => {
       <Link href={"#project"} className="mr-5 hover:text-blue-600">PROJECTS</Link>
       <Link href={"#contact"} className="mr-5 hover:text-blue-600">CONTACTS</Link>
     </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-     </button>
+    
   </div>
 </header>
     </div>
