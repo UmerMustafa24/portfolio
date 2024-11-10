d2ffd90ffd660d108a01d134fbef2f8876e962aa
MaryamKhan2001 <maryamkhanhomm@gmail.com>
2024-11-10 17:46:52 +0500

    changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -9,11 +9,11 @@ const Hero = () => {
      <section className="text-gray-600 body-font bg-fixed bg-cover bg-center custom-image">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              I AM 
-              <br className="hidden lg:inline-block" />
-              MARYAM KHAN
-            </h1>
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            I&apos;m
+           <br className="hidden lg:inline-block" />
+           MARYAM KHAN
+          </h1>
             <div className="w-[400px] h-[2px] bg-gray-500"></div>
             <p className="mb-8 leading-relaxed">
           
